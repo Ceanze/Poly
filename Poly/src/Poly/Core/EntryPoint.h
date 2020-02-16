40c2c134b88ed7becee1e3dd021011e027652b92
Ceanze <simnylen@gmail.com>
2020-02-16 16:16:20 +0100

    Added memory leak check

--- Poly/src/Poly/Core/EntryPoint.h
+++ Poly/src/Poly/Core/EntryPoint.h
@@ -1,10 +1,13 @@
 #pragma once
 #include <windows.h>
+#include <crtdbg.h>
 
 extern Poly::Application* Poly::CreateApplication();
 
 int main()
 {
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
 	Poly::Application* app = Poly::CreateApplication();
 
 	app->run();
