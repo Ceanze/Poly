f34850923ee1979099dfb5f542ece3d3095ce5a9
Ceanze <simnylen@gmail.com>
2020-03-28 15:35:44 +0100

    Added __VA_ARGS__

--- Poly/src/Poly/Core/PolyUtils.h
+++ Poly/src/Poly/Core/PolyUtils.h
@@ -6,8 +6,8 @@
 // PVK prefix is used for vulkan specific macros
 // POLY prefix is used for general macros
 
-// Check if a native vulkan function was successful. F is function, E is error message.
-#define PVK_CHECK(F, E) if(F != VK_SUCCESS) { POLY_CORE_ERROR(E); throw std::runtime_error(E); }
+// Check if a native vulkan function was successful. First param is function, second is error message.
+#define PVK_CHECK(F, ...) if(F != VK_SUCCESS) { POLY_CORE_ERROR(__VA_ARGS__); assert(F); }
 
 // Cleanup of vectors of PVK types
 #define PVK_VEC_CLEANUP(V) for (auto& e : V) { e.cleanup(); }
