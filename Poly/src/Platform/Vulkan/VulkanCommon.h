108cf57047a7a2eef8bb35c2f3bcb5f317775428
Ceanze <simnylen@gmail.com>
2020-03-28 17:28:35 +0100

    Added find memory function

--- Poly/src/Platform/Vulkan/VulkanCommon.h
+++ Poly/src/Platform/Vulkan/VulkanCommon.h
@@ -89,4 +89,18 @@ namespace Poly {
 		return buffer;
 	}
 
+	static uint32_t findMemoryType(VkPhysicalDevice physicalDevice, uint32_t typeFilter, VkMemoryPropertyFlags properties)
+	{
+		VkPhysicalDeviceMemoryProperties memProperties;
+		vkGetPhysicalDeviceMemoryProperties(physicalDevice, &memProperties);
+
+		for (uint32_t i = 0; i < memProperties.memoryTypeCount; i++) {
+			if ((typeFilter & (1 << i)) && (memProperties.memoryTypes[i].propertyFlags & properties) == properties) {
+				return i;
+			}
+		}
+
+		POLY_ASSERT(false, "Failed to find suitable memory type!");
+		return 0;
+	}
 }
