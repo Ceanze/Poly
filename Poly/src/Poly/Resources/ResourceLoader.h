07393c284bbfdde81fb50168fb56b931237ff8de
Ceanze <simnylen@gmail.com>
2020-11-22 19:59:05 +0100

    Began on resourceLoader - need place to init static classes

---
+++ Poly/src/Poly/Resources/ResourceLoader.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+#include "Poly/Rendering/Core/API/GraphicsTypes.h"
+
+namespace Poly
+{
+	class ResourceLoader
+	{
+	public:
+		ResourceLoader() = default;
+		~ResourceLoader() = default;
+
+		static void Init();
+		static void Release();
+
+		static std::vector<char> LoadShader(const std::string& path, ShaderStage shaderStage);
+
+	private:
+		static bool s_GLSLInit;
+	};
+}
