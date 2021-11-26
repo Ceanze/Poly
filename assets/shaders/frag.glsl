d472c4453ef16c0605bd8ab08812c2ed3287a37b
Ceanze <simnylen@gmail.com>
2021-11-26 19:26:55 +0100

    cleanuped shaders

--- assets/shaders/frag.glsl
+++ assets/shaders/frag.glsl
@@ -1,8 +1,24 @@
 #version 450
 #extension GL_ARB_separate_shader_objects : enable
 
+layout(location = 0) in flat uint materialIndex;
 layout(location = 1) in vec2 fragUV;
-layout(location = 2) in vec4 albedoColor;
+
+layout(location = 0) out vec4 outColor;
+
+struct MaterialValues
+{
+	vec4	Albedo;
+	float	AO;
+	float	Metallic;
+	float	Roughness;
+	float	_UNUSED;
+};
+
+layout(set = 2, binding = 0) buffer MaterialProperties
+{
+	MaterialValues material[];
+};
 
 layout(set = 2, binding = 1) uniform sampler2D albedoTex;
 layout(set = 2, binding = 2) uniform sampler2D metalicTex;
@@ -10,9 +26,6 @@ layout(set = 2, binding = 3) uniform sampler2D normalTex;
 layout(set = 2, binding = 4) uniform sampler2D roughnessTex;
 layout(set = 2, binding = 5) uniform sampler2D aoTex;
 
-
-layout(location = 0) out vec4 outColor;
-
 void main() {
-	outColor = texture(albedoTex, fragUV) * albedoColor;
+	outColor = texture(albedoTex, fragUV) * material[materialIndex].Albedo;
 }
--- assets/shaders/vert.glsl
+++ assets/shaders/vert.glsl
@@ -1,8 +1,8 @@
 #version 450
 #extension GL_ARB_separate_shader_objects : enable
 
+layout(location = 0) out flat uint materialIndex;
 layout(location = 1) out vec2 fragUV;
-layout(location = 2) out vec4 albedoColor;
 
 struct Vertex
 {
@@ -11,16 +11,6 @@ struct Vertex
     vec4 Tangent;
     vec4 TexCoord;
 };
-
-struct MaterialValues
-{
-	vec4	Albedo;
-	float	AO;
-	float	Metallic;
-	float	Roughness;
-	float	_UNUSED;
-};
-
 
 layout(set = 0, binding = 0) uniform Camera
 {
@@ -37,13 +27,8 @@ layout(set = 1, binding = 1) buffer Transforms
     mat4 transform[];
 };
 
-layout(set = 2, binding = 0) buffer MaterialProperties
-{
-	MaterialValues material[];
-};
-
 void main() {
     gl_Position = camera * transform[gl_InstanceIndex] * vec4(vertex[gl_VertexIndex].Position.xyz, 1.0);
     fragUV = vertex[gl_VertexIndex].TexCoord.xy;
-    albedoColor = material[gl_InstanceIndex].Albedo;
+    materialIndex = gl_InstanceIndex;
 }
